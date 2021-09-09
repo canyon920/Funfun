455eff57ded5d8077efa40b4bd6cabde7d5649f8
hyelim <limit1633020@gmail.com>
2021-09-09 18:15:14 +0900

    before swiper

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-
 const routes = [
   {
     path: '/',
@@ -18,7 +17,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+    {
+      path: '/main',
+      name: 'Main',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/Main.vue')
+    }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Main.vue
@@ -0,0 +1,108 @@
+<template>
+<div id="first">
+<!-- 툴바 -->
+<v-carousel
+    cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        :color="colors[i]"
+        height="100%"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="text-h2">
+            {{ slide }} Slide
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+<v-container>
+<!-- 사이즈 조정 디브  -->
+<div class = "second">
+<div class = "search">
+친구를 검색해 주세요.
+ <table bgcolor="#FFFFFF">
+<tr>
+ <td>
+  <input type=text name=q size=25 maxlength=255 value="" />
+ <input type=submit name=btnG value="Google 검색" />
+ </td>
+</tr>
+</table>
+
+
+<p>OO님의 위시리스트</p>
+<p>펀딩중인 상품이 없어요</p>
+<v-btn  depressed elevation="2">♡친구에게 보여주기</v-btn>
+</div>
+<div class = "menu">
+메뉴 문구
+</div>
+<div class = "event">
+이벤트 상품
+</div>
+<div class = "deadline">
+마감예정인 상품
+</div>
+<div class = "join">
+내가 참여한 선물
+</div>
+<div class = "giboo">
+나눔을 전하세요
+</div>
+</div>
+</v-container>
+</div>
+</template>
+<script>
+
+
+  export default {
+    name: 'Main',
+  data () {
+        return {
+         model: 0,
+           colors: [
+            'indigo',
+            'warning',
+            'pink darken-2',
+            'red lighten-1',
+            'deep-purple accent-4',
+           ],
+            slides: [
+                     '광고',
+                     '광고',
+                     '광고',
+                     '광고',
+                     '광고',
+                   ],
+                   }
+        },
+    components: {
+
+
+    },
+  }
+
+
+</script>
+<style>
+.second{
+ width: 1100px;
+}
+.v-window v-item-group theme--dark v-carousel{
+height:250;
+}
+
+</style>
