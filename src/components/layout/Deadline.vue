d29bb30a444002152c327285c43c66e69344ab69
hyelim <limit1633020@gmail.com>
2021-09-13 11:57:39 +0900

    vuetify 다시 설치 이전 커밋

---
+++ src/components/layout/Deadline.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <carousel :autoplay="true" :nav="false">
+    <template slot="prev"><span class="prev">prev</span></template>
+    <img src="https://placeimg.com/200/200/any?1">
+
+    <img src="https://placeimg.com/200/200/any?2">
+
+    <img src="https://placeimg.com/200/200/any?3">
+
+    <img src="https://placeimg.com/200/200/any?4">
+    <template slot="next"><span class="next">next</span></template>
+  </carousel>
+</template>
+<script>
+
+import carousel from 'vue-owl-carousel'
+
+export default {
+  components: { carousel },
+}
+
+</script>
