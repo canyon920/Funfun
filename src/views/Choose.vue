33fe873e2ad466437b4dc5cd55995fa1f7e8fcc1
hyelim <limit1633020@gmail.com>
2021-09-28 12:54:16 +0900

    master branch 머지 전 커밋

--- src/views/Choose.vue
+++ src/views/Choose.vue
@@ -1,35 +1,99 @@
 <template>
 <div class = "first">
   <div class = "second">
-    <v-btn
+    <div  v-if="give" class ="second-first">
+    <div class = "second inner">
+      <v-text-field
+          label="이름"
+          outlined
+          dense
+      ></v-text-field>
+      <v-text-field
+          label="계좌번호"
+          outlined
+          dense
+      ></v-text-field>
+      <v-btn
+          class="ma-2"
+          outlined
+          color="indigo"
+      >
+        확인
+      </v-btn>
+    </div>
+    </div>
+    <div  v-else-if="givepresent" class ="second-second">
+      <v-text-field
+          label="이름"
+          outlined
+          dense
+      ></v-text-field>
+      <v-text-field
+          label="주소"
+          outlined
+          dense
+      ></v-text-field>
+      <v-text-field
+          label="전화번호"
+          outlined
+          dense
+      ></v-text-field>
+      <v-btn
+          class="ma-2"
+          outlined
+          color="indigo"
+      >
+        확인
+      </v-btn>
+    </div>
+
+    <div v-else class = "choose-button">
+    <v-btn @click="onClickButton1()"
         class="ma-2"
         outlined
         color="indigo"
     >
-    계좌로받기
+      계좌로 받기
     </v-btn>
-    <v-btn
+    <v-btn @click="onClickButton2()"
         class="ma-2"
         outlined
         color="indigo"
     >
-      상품으로 받기
+      상품으로받기
     </v-btn>
+    </div>
+  </div>
+  <div class="second-bottom">
+
   </div>
 </div>
 </template>
 <script>
+import axios from "axios";
 
 export default {
   name: 'Choose',
   data(){
     return{
-
+       give:false,
+      givepresent:false,
     }
-  }
+  },
+  methods:{
+    onClickButton1(){
+
+      this.give = true
+    },
+    onClickButton2(){
+      this.givepresent = true
+    }
+  },
+
 }
 
 </script>
+
 <style>
 .first{
   max-width:1100px; left:0; right:0; margin-left:auto; margin-right:auto;
